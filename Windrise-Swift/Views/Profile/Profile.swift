4d503307f044a3c27520b8c7539a52da021a4fa0
Bryan <71095749+bryanless@users.noreply.github.com>
2022-06-01 16:51:42 +0700

    Improve Favorite Characters section  UI

--- Windrise-Swift/Views/Profile/Profile.swift
+++ Windrise-Swift/Views/Profile/Profile.swift
@@ -13,10 +13,40 @@ struct Profile: View {
     @FetchRequest(
         sortDescriptors: [SortDescriptor(\.id)],
         predicate: NSPredicate(format: "isFavorite = true")) private var favoriteCharacters: FetchedResults<FavoriteCharacter>
+    @EnvironmentObject private var mainViewModel: MainViewModel
+    
+    private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     
     var body: some View {
-        List(favoriteCharacters) { character in
-            Text(character.id ?? "Unknown")
+        //        List(favoriteCharacters) { character in
+        //            Text(character.id ?? "Unknown")
+        //        }
+        
+        NavigationView {
+            ScrollView (showsIndicators: false) {
+                VStack (alignment: .leading) {
+                    Text("Favorite Characters")
+                        .font(.title2)
+                    
+                    if (mainViewModel.characters.isEmpty) {
+                        ProgressView()
+                    } else {
+                        LazyVGrid(columns: columns) {
+                            ForEach(favoriteCharacters, id: \.self) { character in
+                                if let index = mainViewModel.ids.firstIndex(of: character.id ?? "0") {
+                                    NavigationLink(destination: CharacterDetail(id: mainViewModel.ids[index], character: mainViewModel.characters[index])) {
+                                        CharacterItem(id: mainViewModel.ids[index], character: mainViewModel.characters[index])
+                                    }
+                                    .tag(mainViewModel.ids[index])
+                                    .buttonStyle(PlainButtonStyle())
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Profile")
         }
     }
 }
@@ -24,5 +54,6 @@ struct Profile: View {
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
         Profile()
+            .environmentObject(MainViewModel())
     }
 }
